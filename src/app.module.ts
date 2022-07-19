b4a32a4c5f789520f1642df3c494ed110f4b7e92
postBG <profile2697@gmail.com>
2022-07-20 00:32:48 +0900

    feat: install nodemailer and create email app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { EmailService } from './email/email.service';
 
 @Module({
   imports: [UsersModule],
+  providers: [EmailService],
 })
 export class AppModule {}
