20d6f010b5003dfed35363a3de3a68104c357d51
postBG <profile2697@gmail.com>
2022-07-22 02:07:19 +0900

    feat: install class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsString, MinLength, MaxLength, IsEmail } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(20)
   readonly name: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsString()
   readonly password: string;
 }
