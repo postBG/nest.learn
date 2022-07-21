53e51388c483adbe683d20a44bd6e1b434c9dae5
postBG <profile2697@gmail.com>
2022-07-22 00:51:24 +0900

    refactor: to use emailConfig (DynamicConfig)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { EmailModule } from './email/email.module';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
