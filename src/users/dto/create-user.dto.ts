f12e16f484419e2f3339599374e977752bd77a17
postBG <profile2697@gmail.com>
2022-07-25 01:35:35 +0900

    feat: add a new rule containing name and password should not be same

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -6,9 +6,17 @@ import {
   MinLength,
 } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { BadRequestException } from '@nestjs/common';
 
 export class CreateUserDto {
-  @Transform((params) => params.value.trim())
+  @Transform(({ value, obj }) => {
+    if (obj.password.includes(value.trim())) {
+      throw new BadRequestException(
+        'password는 name과 같은 문자열을 포함할 수 없습니다.',
+      );
+    }
+    return value.trim();
+  })
   @IsString()
   @MinLength(2)
   @MaxLength(30)
