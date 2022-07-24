8d1bd8c7ea02b602f979611c42104d741596bc39
postBG <profile2697@gmail.com>
2022-07-25 01:59:25 +0900

    refactor: move password should not include name to class-validator decorator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -6,22 +6,19 @@ import {
   MinLength,
 } from 'class-validator';
 import { Transform } from 'class-transformer';
-import { BadRequestException } from '@nestjs/common';
+import { NotIn } from 'src/utils/decorators/not-in';
 
 export class CreateUserDto {
-  @Transform(({ value, obj }) => {
-    if (obj.password.includes(value.trim())) {
-      throw new BadRequestException(
-        'password는 name과 같은 문자열을 포함할 수 없습니다.',
-      );
-    }
-    return value.trim();
+  @Transform((params) => params.value.trim())
+  @NotIn('password', {
+    message: 'password는 name과 같은 문자열을 포함할 수 없습니다.',
   })
   @IsString()
   @MinLength(2)
   @MaxLength(30)
   readonly name: string;
 
+  @Transform((params) => params.value.trim())
   @IsString()
   @IsEmail()
   @MaxLength(60)
