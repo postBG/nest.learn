594146fc96f73d97a31a78f2932ad6efcf9e2eb8
postBG <profile2697@gmail.com>
2022-08-07 23:57:23 +0900

    feat: set log level

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.NODE_ENV === 'production'
+        ? ['error', 'warn', 'log']
+        : ['error', 'warn', 'log', 'verbose', 'debug'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
