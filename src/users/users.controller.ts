ae9f045d0d639176fff561895eb09d030af11e97
postBG <profile2697@gmail.com>
2022-08-08 00:20:21 +0900

    feat: replace nest logger service with winston logger service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,28 +1,21 @@
 import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Query,
-  Headers,
-  UseGuards,
-  Inject,
-} from '@nestjs/common';
+  Body, Controller, Get, Headers, Inject, InternalServerErrorException, LoggerService, Param, Post, Query, UseGuards
+} from "@nestjs/common";
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { AuthService } from '../auth/auth.service';
 import { AuthGuard } from '../auth.guard';
-import { WINSTON_MODULE_PROVIDER, WinstonLogger } from 'nest-winston';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Controller('users')
 export class UsersController {
   constructor(
     private readonly usersService: UsersService,
     private authService: AuthService,
-    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: WinstonLogger,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: LoggerService,
   ) {}
 
   @Post()
@@ -41,7 +34,7 @@ export class UsersController {
   @Post('/login')
   async login(@Body() dto: UserLoginDto): Promise<string> {
     const { email, password } = dto;
-    this.printWinstonLog(dto);
+    this.printLoggerServiceLog(dto);
     return await this.usersService.login(email, password);
   }
 
@@ -58,10 +51,15 @@ export class UsersController {
     return this.usersService.getUserInfo(userId);
   }
 
-  private printWinstonLog(dto) {
-    this.logger.error('error: ', dto);
-    this.logger.warn('warn: ', dto);
-    this.logger.verbose('verbose: ', dto);
-    this.logger.debug('debug: ', dto);
+  private printLoggerServiceLog(dto) {
+    try {
+      throw new InternalServerErrorException('test');
+    } catch (e) {
+      this.logger.error('error: ' + JSON.stringify(dto), e.stack);
+    }
+    this.logger.warn('warn: ' + JSON.stringify(dto));
+    this.logger.log('log: ' + JSON.stringify(dto));
+    this.logger.verbose('verbose: ' + JSON.stringify(dto));
+    this.logger.debug('debug: ' + JSON.stringify(dto));
   }
 }
